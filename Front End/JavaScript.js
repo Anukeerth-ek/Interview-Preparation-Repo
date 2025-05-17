c5df94c6fffa908d1a94cd46c578c54dd715f265
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-05-17 11:42:44 +0530

    feat: added new js topics

--- Front End/JavaScript.js
+++ Front End/JavaScript.js
@@ -460,3 +460,16 @@
 // Both of them are used to increment the value, and if we are using i++ we are first assigning and then incrementing the value of i by 1, and if we are using ++i we are first 
 // incrementing and then assigning that to the value i, 
 // _________________________________
+
+// _________________________________
+// 23. In which states can a Promise be?
+// ans:
+// A Promise is in one of these states:
+
+// pending: initial state, neither fulfilled nor rejected.
+// fulfilled: meaning that the operation completed successfully.
+// rejected: meaning that the operation failed.
+
+// A pending promise can either be fulfilled with a value, or rejected with a reason (error). When either of these options happens, the associated handlers queued up by a promise's 
+// then method are called.
+// _________________________________
